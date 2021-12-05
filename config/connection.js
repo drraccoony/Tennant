f0adabb17a368a3918c025e0668b712116ce1e45
Shawn McHenry <drraccoony@gmail.com>
2021-12-04 21:13:47 -0600

    use

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'us-cdbr-iron-east-02.cleardb.net',
+    host: process.env.DB_URL,
     dialect: 'mysql',
     port: 3306
 })
